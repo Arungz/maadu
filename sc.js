c82dc18852d14240ca11410c7cb6c4407d38d9be
A R U N <65488988+Arungz@users.noreply.github.com>
2023-07-22 02:11:23 +0530

    Add files via upload

--- sc.js
+++ sc.js
@@ -1,15 +1,16 @@
-function validate()
+  function validate()
 {
-    if(   document.getElementById("text1").value == "Karthika"
-       && document.getElementById("text2").value == "enperukarthika" )
+    if(   document.getElementById("text1").value == "0"
+       && document.getElementById("text2").value == "0" )
     {
-        alert( "validation succeeded" );
-        location.href="https://arungz.github.io/maadu/mayilu.html";
+        location.href="mayilu.html";
+    }else if(document.getElementById("text1").value == ""
+    && document.getElementById("text2").value == ""){
+        location.href="https://youtube.com/shorts/XOilrabaXVI?feature=share"; 
     }
     else
     {
-        alert( "validation failed" );
-        location.href="https://youtube.com/shorts/XOilrabaXVI?feature=share";
+        location.href="https://youtu.be/Rxgsl6Wlt04";
     }
 }
     // disable right click
@@ -37,29 +38,5 @@ function validate()
             return false;
         }
     }
-    // disable right click
-    document.addEventListener('contextmenu', event => event.preventDefault());
- 
-    document.onkeydown = function (e) {
- 
-        // disable F12 key
-        if(e.keyCode == 123) {
-            return false;
-        }
- 
-        // disable I key
-        if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
-            return false;
-        }
- 
-        // disable J key
-        if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-            return false;
-        }
- 
-        // disable U key
-        if(e.ctrlKey && e.keyCode == 85) {
-            return false;
-        }
-    }
+
  
