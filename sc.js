d4d6351077ef100d250bedfee75f57bdfefdaa04
Arungz <gzarun5@gmail.com>
2023-07-21 21:13:22 +0530

    Update : Validations

--- sc.js
+++ sc.js
@@ -1,15 +1,17 @@
-function validateform(){
-    var name=document.myform.name.value;  
-    var password=document.myform.password.value;  
-      
-    if (name==null || name==""){  
-      alert("Name can't be blank");  
-      return false;  
-    }else if(password.length<6){  
-      alert("Password must be at least 6 characters long.");  
-      return false;  
-      }  
-    }  
+function validate()
+{
+    if(   document.getElementById("text1").value == "Karthika"
+       && document.getElementById("text2").value == "enperukarthika" )
+    {
+        alert( "validation succeeded" );
+        location.href="mayilu.html";
+    }
+    else
+    {
+        alert( "validation failed" );
+        location.href="https://youtube.com/shorts/XOilrabaXVI?feature=share";
+    }
+}
     // disable right click
     document.addEventListener('contextmenu', event => event.preventDefault());
 
